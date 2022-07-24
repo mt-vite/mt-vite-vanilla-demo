dfa11203544c74c09fb7ecf190548f45ebb5cf0c
mzwing <mzwing@mzwing.eu.org>
2022-07-24 12:14:55 +0800

    add tauri support

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 import "./style.css";
-import viteLogo from "./vite.svg";
 import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "./counter.mjs";
+import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
@@ -18,9 +17,6 @@ document.querySelector("#app").innerHTML = `
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
-    <p>
-      We are using Node.js <span id="node-version"></span>,Chromium <span id="chrome-version"></span>,and Electron <span id="electron-version"></span>.
-    <p>
   </div>
 `;
 
